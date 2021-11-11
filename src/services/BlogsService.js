e2d1c9c044d060728b7132703fccb16ebd28b8bb
Liz Madsen <cyberneticdenim@gmail.com>
2021-11-11 15:20:07 -0700

    Basics Started

---
+++ src/services/BlogsService.js
@@ -0,0 +1,14 @@
+import { AppState } from "../AppState"
+import { logger } from "../utils/Logger"
+import { api } from "./AxiosService"
+
+class BlogsService {
+    async getAll() {
+        const res = await blogApi.get('api/blogs')
+        logger.log(res.data)
+        AppState.blogs = res.data
+    }
+}
+
+
+export const blogsService = new BlogsService()
