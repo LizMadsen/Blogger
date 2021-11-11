93e6d2647842954c0e8d287dd714d2a8e6292f93
Liz Madsen <cyberneticdenim@gmail.com>
2021-11-11 15:19:56 -0700

    Basic Setup

---
+++ src/components/Blog.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="component">
+
+
+    </div>
+</template>
+
+
+<script>
+    import { logger } from '../utils/Logger'
+    // import { Pop } from '../utils/Pop'
+    export default {
+        props: { blog: { type: Object, required: true } },
+        setup() {
+            return {
+                async getAll() {
+                    try {
+                        await blogsService.getAll()
+                    } catch (error) {
+                        logger.error(error)
+                        Pop.toast(error.message, 'Error, man')
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
